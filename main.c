d6bceb347aa909d0c688f98956a3fbf58dfcba5f
LucasWiman90 <lucas.wiman33@gmail.com>
2024-06-04 22:12:12 +0200

    Add player move functionality
    Main loop added.
    The player can now move with w,a,s,d keys.

--- main.c
+++ main.c
@@ -1,15 +1,38 @@
 #include <ncurses.h>
+#include <stdlib.h>
+
+typedef struct Player
+{
+    int xPosition;
+    int yPosition;
+    int health;
+} Player;
 
 int screenSetup();
 int mapSetup();
+Player *playerSetup();
+int handleInput(int input, Player *player);
+int playerMove(int y, int x, Player *player);
 
 int main() 
 {
+    Player *player;
+    int ch;
+
+    //Setup the screen
     screenSetup();
 
+    //Setup the map
     mapSetup();
 
-    getch();
+    //Setup the player
+    player = playerSetup();
+
+    //Main game loop
+    while((ch = getch()) != 'q')
+    {
+        handleInput(ch, player);
+    }
 
     endwin();
 
@@ -23,7 +46,7 @@ int screenSetup()
     noecho();
     refresh();
 
-    return 0;
+    return 1;
 }
 
 int mapSetup()
@@ -48,4 +71,64 @@ int mapSetup()
     mvprintw(13, 40, "|..........|");
     mvprintw(14, 40, "|..........|");
     mvprintw(15, 40, "------------");
+}
+
+Player *playerSetup()
+{
+    Player *newPlayer;
+    newPlayer = malloc(sizeof(Player));
+
+    newPlayer->xPosition = 14;
+    newPlayer->yPosition = 14;
+    newPlayer->health = 20;
+
+    playerMove(14, 14, newPlayer);
+
+    return newPlayer;
+}
+
+int handleInput(int input, Player *player)
+{
+    switch(input)
+    {
+        //Move up
+        case 'w':
+        case 'W':
+            playerMove(player->yPosition-1, player->xPosition, player);
+            break;
+
+        //Move down
+        case 's':
+        case 'S':
+            playerMove(player->yPosition+1, player->xPosition, player);
+            break;
+
+        //Move left
+        case 'a':
+        case 'A':
+            playerMove(player->yPosition, player->xPosition-1, player);
+            break;
+
+        //Move right
+        case 'd':
+        case 'D':
+            playerMove(player->yPosition, player->xPosition+1, player);
+            break;
+
+        default:
+            break;
+    }
+}
+
+//Move the player, replace old position with floor.
+//Update position and draw the player in the new position and move the cursor
+int playerMove(int y, int x, Player *player)
+{
+    mvprintw(player->yPosition, player->xPosition, ".");
+
+    player->yPosition = y;
+    player->xPosition = x;
+
+    mvprintw(player->yPosition, player->xPosition, "@");
+    move(player->yPosition,player->xPosition);
 }
