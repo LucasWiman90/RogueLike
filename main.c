65f722a7284134adaac86ce715eb14e5c013c799
LucasWiman90 <lucas.wiman33@gmail.com>
2024-06-06 20:43:01 +0200

    Update room handling
    - Define a struct for rooms
    - Add room related functions
    - Add a safe_malloc function

--- main.c
+++ main.c
@@ -1,30 +1,47 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+typedef struct Room
+{
+    int yPosition;
+    int xPosition;
+    int height;
+    int width;
+    //Monster ** monsters;
+    //Item ** items;
+} Room;
+
 typedef struct Player
 {
     int xPosition;
     int yPosition;
     int health;
+    // Room * room;
 } Player;
 
+void *safe_malloc(size_t size);
 int screenSetup();
-int mapSetup();
+Room ** mapSetup();
 Player *playerSetup();
 void handleInput(int input, Player *player);
 void playerMove(int y, int x, Player *player);
 void checkPosition(int newY, int newX, Player *player);
 
+//Room functions
+Room * createRoom(int y, int x, int height, int width);
+void drawRoom(Room *room);
+
 int main() 
 {
     Player *player;
+    Room ** rooms;
     int ch;
 
     //Setup the screen
     screenSetup();
 
     //Setup the map
-    mapSetup();
+    rooms = mapSetup();
 
     //Setup the player
     player = playerSetup();
@@ -56,34 +73,103 @@ int screenSetup()
 }
 
 /**
- * @brief Draws the map
+ * @brief Initializes and sets up the map with rooms.
  * @param None
- * @return Returns 0 when succesful
+ * @return Returns a pointer to an array of Room pointers when succesful
  * */
-int mapSetup()
+Room ** mapSetup()
 {
-    mvprintw(13, 13, "--------");
-    mvprintw(14, 13, "|......|");
-    mvprintw(15, 13, "|......|");
-    mvprintw(16, 13, "|......|");
-    mvprintw(17, 13, "|......|");
-    mvprintw(18, 13, "--------");
+    Room ** rooms;
+    rooms = safe_malloc(sizeof(Room) * 6);
 
-    mvprintw(2, 40, "--------");
-    mvprintw(3, 40, "|......|");
-    mvprintw(4, 40, "|......|");
-    mvprintw(5, 40, "|......|");
-    mvprintw(6, 40, "|......|");
-    mvprintw(7, 40, "--------");
+    rooms[0] = createRoom(13, 13, 6, 8);
+    drawRoom(rooms[0]);
 
-    mvprintw(10, 40, "------------");
-    mvprintw(11, 40, "|..........|");
-    mvprintw(12, 40, "|..........|");
-    mvprintw(13, 40, "|..........|");
-    mvprintw(14, 40, "|..........|");
-    mvprintw(15, 40, "------------");
+    rooms[1] = createRoom(2, 40, 6, 8);
+    drawRoom(rooms[1]);
 
-    return 0;
+    rooms[2] = createRoom(10, 40, 6, 12);
+    drawRoom(rooms[2]);
+
+    return rooms;
+}
+
+/**
+ * @brief Allocates memory with error checking.
+ *
+ * This function allocates memory using malloc and checks if the allocation
+ * was successful. If malloc fails and the requested size is greater than 0,
+ * it prints an error message using perror and exits the program with failure.
+ *
+ * @param size The size of the memory block to allocate.
+ * @return A pointer to the allocated memory block, or NULL if the allocation fails.
+ */
+void *safe_malloc(size_t size)
+{
+    void *ptr = malloc(size);
+
+    if (!ptr && (size > 0)) {
+      perror("malloc failed!");
+      exit(EXIT_FAILURE);
+    }
+
+    return ptr;
+}
+
+/**
+ * @brief Creates a room.
+ *
+ * This function creates a room with x,y coordinates (top left corner).
+ * And with a defined width and height.
+ *
+ * @param size The size of the memory block to allocate.
+ * @return A pointer to the newly created room.
+ */
+Room * createRoom(int y, int x, int height, int width)
+{
+    Room * newRoom;
+    newRoom = safe_malloc(sizeof(Room));
+
+    newRoom->yPosition = y;
+    newRoom->xPosition = x;
+    newRoom->height = height;
+    newRoom->width = width;
+
+    return newRoom;
+}
+
+/**
+ * @brief Draws a room.
+ *
+ * This function draws a room
+ *
+ * @param room A pointer to a room to be drawn.
+ */
+void drawRoom(Room *room)
+{
+    int y;
+    int x;
+
+    //Draw top and bottom of room
+    for (x = room->xPosition; x < room->xPosition + room->width; x++)
+    {
+        mvprintw(room->yPosition, x, "-"); /*top*/
+        mvprintw(room->yPosition + room->height - 1, x, "-"); /*bottom*/
+    }
+
+    //Draw the floors and sidewalls
+    for(y = room->yPosition + 1; y < room->yPosition + room->height - 1; y++)
+    {
+        /*draw side walls*/
+        mvprintw(y, room->xPosition, "|");
+        mvprintw(y, room->xPosition + room->width - 1, "|");
+
+        /*draw floors*/
+        for(x = room->xPosition + 1; x < room->xPosition + room->width - 1; x++)
+        {
+            mvprintw(y, x, ".");
+        }
+    }
 }
 
 /**
@@ -94,7 +180,7 @@ int mapSetup()
 Player* playerSetup()
 {
     Player *newPlayer;
-    newPlayer = malloc(sizeof(Player));
+    newPlayer = safe_malloc(sizeof(Player));
 
     newPlayer->xPosition = 14;
     newPlayer->yPosition = 14;
